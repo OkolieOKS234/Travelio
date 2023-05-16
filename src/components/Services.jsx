37b5d9cfb4f98a02c9e3b0f9a1529b5b8283cd7b
OkolieOKS234 <dokolie040@gmail.com>
2023-05-16 11:45:12 +0100

    added icons for a section

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,19 +8,24 @@ const Services = () => {
   return (
     <section>
         <h3 className='text-center font-bold text-2xl'>WE OFFER THE BEST SERVICES</h3>
-<div className='flex justify-center items-center'>
+<div className='flex justify-center items-center gap-4'>
     <div className='mt-12'>
-        <img src={Weather} alt="" />
+        <img src={Weather} alt="" className='w-24' />
        <p className='text-center w-24 text-sm'>Experience the perfect travel with our travel agency</p> 
     </div>
-    <div className='rounded'>
-    <img src={Flights} alt="" className='mt-2' />
-    </div>
+   
     <div>
+    <img src={Flights} alt="" className=' w-28 mt-6' />
+    <p className='text-center w-24 text-sm'>Book hotels online and book flights online</p>
+    </div>
+
+    <div  className=' w-28 mt-12' >
     <img src={LocalEvents} alt="" />
+    <p className='text-center w-24 text-sm'>Book hotels online and book flights online</p>
     </div>
     <div>
-    <img src={Customization} alt="" />
+    <img src={Customization} alt="" className=' w-24 mt-6'  />
+    <p className='text-center w-24 text-sm'>Customize your travel package</p>
     </div>
 </div>
 
